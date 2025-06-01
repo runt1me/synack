af2b35f3171c56a46c78e169e52b9e40301e1251
Ryan Thomas <rtchess.maniac@gmail.com>
2025-05-31 21:14:34 -0400

    add powerline config to ubuntu_setup

--- ubuntu_setup.bash
+++ ubuntu_setup.bash
@@ -1,3 +1,23 @@
+#!/bin/bash
 gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier '<Alt>'
 gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
 
+add-apt-repository universe
+apt install --yes powerline
+
+# Define the lines to add to .bashrc
+lines="# Powerline configuration
+if [ -f /usr/share/powerline/bindings/bash/powerline.sh ]; then
+  powerline-daemon -q
+  POWERLINE_BASH_CONTINUATION=1
+  POWERLINE_BASH_SELECT=1
+  source /usr/share/powerline/bindings/bash/powerline.sh
+fi"
+
+# Check if the lines are already in .bashrc to avoid duplication
+if ! grep -Fxq "$lines" ~/.bashrc; then
+    echo "$lines" >> ~/.bashrc
+    echo "Powerline configuration added to .bashrc."
+else
+    echo "Powerline configuration already present in .bashrc."
+fi
